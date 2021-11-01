9f2faa37e4d1af5551f6217b9b0a552f0c218c72
lesrz <suarezarnaldi.leo@gmail.com>
2021-11-01 17:02:39 -0300

    Añadidos: menu y sonidos

--- Seminario-de-Lenguajes/colateral-21/game.py
+++ Seminario-de-Lenguajes/colateral-21/game.py
@@ -25,13 +25,17 @@ WIDTH = 800
 HEIGHT = 600
 
 BLACK = (15, 15, 25)
+GREY = (55,55,55)
 WHITE = (255, 255, 255)
 GREEN = (0, 180, 0)
 RED = (200, 0, 0)
 
 LIFEBAR_WIDTH = 3
-MISSILE_SPEED = 10
-MISSILE_DMG = 4
+MISSILE_SPEED = 6
+MISSILE_DMG = 3
+
+SFX_VOLUME = 0.5
+BGM_VOLUME = 0.2
 #endregion
 
 #region Pantalla
@@ -142,10 +146,11 @@ class Enemy():
         self.dx = random.randint(10, 50) / -10
         self.dy = 0
         self.surface = pygame.image.load('assets/enemy.png').convert_alpha()
+        self.rect = self.surface.get_rect()
         self.max_health = random.randint(5, 15)
         self.health = self.max_health
         self.type = 'enemy'
-        
+
 
     def move(self):
         self.x = self.x + self.dx
@@ -170,7 +175,7 @@ class Enemy():
 
     def render(self):
         screen.blit(self.surface, (int(self.x), int(self.y)))
-        
+
         # Randeriza barra de vida
         pygame.draw.line(screen, RED, (int(self.x), int(self.y)), (int(self.x + (30 * (self.health/self.max_health))), int(self.y)), LIFEBAR_WIDTH)
 #endregion
@@ -199,13 +204,59 @@ class Star():
         screen.blit(self.surface, (int(self.x), int(self.y)))
 #endregion
 
+#region Menu
+
+def draw_text(surface, text, size, x, y, color):
+	font = pygame.font.Font('assets/font/kenvector_future_thin.ttf', size)
+	text_surface = font.render(text, True, color)
+	text_rect = text_surface.get_rect()
+	text_rect.midtop = (x, y)
+	surface.blit(text_surface, text_rect)
+
+def show_menu():
+    screen.blit(screen, [0,0])
+    draw_text(screen, "COLATERAL-21", 65, WIDTH//2, HEIGHT//4, RED)
+    draw_text(screen, "CONTROLES", 28, WIDTH // 2, HEIGHT // 2.3, WHITE)
+    draw_text(screen, "Movimiento: FLECHAS", 18, WIDTH // 2, HEIGHT // 1.9, WHITE)
+    draw_text(screen, "Disparo:   BARRA ESP.", 18, WIDTH // 2, HEIGHT // 1.8, WHITE)
+    draw_text(screen, "Presione cualquier tecla", 12, WIDTH // 2, HEIGHT * 3/4, WHITE)
+    draw_text(screen, "Hecho por", 10, WIDTH // 2, HEIGHT * 3.5/4, WHITE)
+    draw_text(screen, "Suarez Arnaldi, Leonel", 10, WIDTH // 2, HEIGHT * 3.56/4, GREY)
+    draw_text(screen, "Moro, Enzo Sebastian", 10, WIDTH // 2, HEIGHT * 3.62/4, GREY)
+    draw_text(screen, "Luoni, Brian Sebastian", 10, WIDTH // 2, HEIGHT * 3.68/4, GREY)
+    draw_text(screen, "Guevara, Agustin Ezequiel", 10, WIDTH // 2, HEIGHT * 3.74/4, GREY)
+    draw_text(screen, "Fernandez, Nahuel", 10, WIDTH // 2, HEIGHT * 3.8/4, GREY)
+    draw_text(screen, "Cruz, Denis Liam", 10, WIDTH // 2, HEIGHT * 3.86/4, GREY)
+    pygame.display.flip()
+    waiting = True
+    while waiting:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYUP:
+                waiting = False
+
+
+#endregion
+
 #region SFX & VFX
 # Crea sonidos
+bgm = pygame.mixer.Sound('assets/bgm.wav')
+bgm.set_volume(BGM_VOLUME)
+
 missile_sound = pygame.mixer.Sound('assets/missile.ogg')
 explosion_sound = pygame.mixer.Sound('assets/explosion.wav')
+kill_sound = pygame.mixer.Sound('assets/kill.wav')
+crash_sound = pygame.mixer.Sound('assets/crash.wav')
+explosion_sound.set_volume(SFX_VOLUME - 0.3)
+missile_sound.set_volume(SFX_VOLUME + 0.4)
+kill_sound.set_volume(SFX_VOLUME)
+crash_sound.set_volume(SFX_VOLUME - 0.2)
 
 # Crea fuente
-font = pygame.font.SysFont("agencyfb", 24)
+#font = pygame.font.SysFont("agencyfb", 24)
+font = pygame.font.Font('assets/font/kenvector_future.ttf', 16)
 #endregion
 
 #region Objetos
@@ -226,15 +277,22 @@ def fire_missile():
     for missile in missiles:
         if missile.state == "ready":
             missile.fire()
-            missile_sound.play()
+            pygame.mixer.Channel(1).play(missile_sound)
             break
 #endregion
 
 ####LOOP PRINCIPAL###
 #region Main
-while True:
+game_over = True
+running = True
+while running:
+    if game_over:
+        show_menu()
+        game_over = False
+    bgm.play(-1)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
+            running = False
             sys.exit()
             
         # Controles
@@ -265,9 +323,11 @@ while True:
         # Chequeo de colisión
         for missile in missiles:
             if enemy.distance(missile) < 20:
-                explosion_sound.play()
+                #explosion_sound.play()
+                pygame.mixer.Channel(0).play(explosion_sound)
                 enemy.health -= MISSILE_DMG
                 if enemy.health <= 0:
+                    pygame.mixer.Channel(3).play(kill_sound)
                     enemy.x = random.randint(800, 900)
                     enemy.y = random.randint(0, 550)
                     
@@ -276,8 +336,8 @@ while True:
                     if player.kills >= 50:
                         BLACK = (25, 10, 25)
                         missile.image = pygame.image.load('assets/missileM.png').convert_alpha()
-                        missile.speed = MISSILE_SPEED*1.5
-                        MISSILE_DMG = MISSILE_DMG*1.1
+                        missile.speed = 10
+                        MISSILE_DMG = 4.5
                     if player.kills % 10 == 0:
                         enemy.surface = pygame.image.load('assets/boss.png').convert_alpha()
                         enemy.max_health = 50
@@ -304,7 +364,7 @@ while True:
         
         # Chequeo de colisión
         if enemy.distance(player) < 20:
-            explosion_sound.play()
+            pygame.mixer.Channel(2).play(crash_sound)
             
             player.health -= random.randint(5, 10)
             enemy.health -= random.randint(5, 10)
@@ -314,12 +374,12 @@ while True:
             if player.health <= 0:
                 print("GAME OVER")
                 pygame.quit()
-                exit()    
+                exit()
 
     # Randerizar
     # Llena el fondo de color
     screen.fill(BLACK)
-     
+    
     # Randeriza estrellas
     for star in stars:
         star.render()
@@ -340,13 +400,16 @@ while True:
             ammo += 1
     
     for x in range(ammo):
-        screen.blit(missile.image, (700 + 30 * x, 20))
+        screen.blit(missile.image, (630 + 30 * x, 40))
     
     # Randerizar contador
-    score_surface = font.render(f"Score: {player.score} Kills: {player.kills}", True, WHITE)
-    screen.blit(score_surface, (500, 5))
+    score_surface = font.render(f"Score: {player.score}   Kills: {player.kills}", True, WHITE)
+    ammo_surface = font.render(f"Ammo: ", True, WHITE)
+    screen.blit(score_surface, (550, 10))
+    screen.blit(ammo_surface, (550, 35))
     
     pygame.display.flip()
     
     clock.tick(60)
+
 #endregion
